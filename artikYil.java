1eb17930b695e734443e307894cf142468d7648d
muhammed <alaa150alaa@.com>
2022-06-14 15:06:16 +0300

    tam

--- artikYil.java
+++ artikYil.java
@@ -36,3 +36,15 @@ public class artikYil {
         }
     }
 }
+/* 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024 gibi.
+100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
+
+Örneğin 1200, 1600, 2000 yılları artık yıldır ancak 1700, 1800 ve 1900 artık yıl değildir.
+Sadece 400'e tam olarak bölünebilenlerin artık yıl kabul edilmesinin nedeni, bir astronomik yılın 365,25 gün değil, yaklaşık olarak 365,242 gün olmasından kaynaklanan hatayı gidermektir.
+
+Senaryo
+
+Yıl Giriniz : 2020
+2020 bir artık yıldır !
+Yıl Giriniz : 2021
+2021 bir artık yıldır değildir ! */
